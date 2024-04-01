23777995cd7e38add1da66ae9ff4c81e1616498c
Liam <liamthorne4@gmail.com>
2024-04-01 21:15:41 +0100

    Need to implement t_working loop check for counter example

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,77 @@
-fn colatz(n: u64, filter: &u64) -> (bool, u64) {
-    // Create new filter not update original filter if counter_example found
-    let mut n_filter: u64 = filter.clone();
-    let mut next_n: u64 = n;
+use std::collections::HashSet;
 
-    loop {
-        // All values smaller than n have been checked and powers of 2 always go straight to 1-4 loop
-        if next_n < n || (next_n & (next_n - 1)) == 0 {
-            return (false, n_filter)
+fn colatz_new(n: u64, mut t_main: HashSet<u64>) -> (bool, HashSet<u64>) {
+    /* Proven:
+     * 1) If n is even:
+     *   a) If n_prime is in t_main, replace n_prime with n in t_main (even single-operation principle)
+     *   b) If n_prime is not in t_main, n_prime is already a full node in T
+     * 2) If n is odd:
+     *   a) If n_prime is in t_main, n is already a full node in T (odd multiple-operation principle)
+     *   b) If n_prime is not in t_main, ...
+     * 3) Need to prove case where loop outside of main tree exists
+     */
 
-        // If next_n == n, a graph cycle has been found
-        } else if next_n == n {
-            return (true, filter.clone())
+    let mut n_prime = if (n & 1) == 1 { 3 * n + 1 } else { n >> 1 };
+    
+    // n_prime in t_main or n_prime is in 2^m trunk
+    if (n_prime & (n_prime - 1)) == 0 || t_main.contains(&n_prime) {
 
-        } else {
-            n_filter |= next_n;
-            next_n = if (next_n & 1) == 1 { 3 * next_n + 1 } else { next_n >> 1 };
+        // n is even => max 1 operation conducted => replace n_prime with n
+        if n & 1 == 0 {
+            t_main.remove(&n_prime);
+            t_main.insert(n);
+        };
+        // n is odd => n is already a full node in T => do nothing
+
+    // n is even and n_prime not in t_main => n_prime already a full node in T
+    } else if n & 1 == 0 {
+
+    // n is odd and n_prime not in t_main, create new tree
+    } else {
+        let mut n_curr = n;
+        let mut t_working = HashSet::from([n]);
+
+        loop {
+            // If non-full n_prime in t_main or n_prime is in 2^m trunk, replace it with t_working
+            if (n_prime & (n_prime - 1)) == 0 || t_main.contains(&n_prime) {
+                t_main.remove(&n_prime);              
+                t_main.extend(&t_working);
+                break;
+
+            // n_curr is odd => add n_prime to t_working 
+            } else if n_curr & 1 == 1 {
+                t_working.insert(n_prime);
+            }
+
+            // n_curr is even => parent will be full => do nothing
+            n_curr = n_prime;
+            n_prime = if (n_prime & 1) == 1 { 3 * n_prime + 1 } else { n_prime >> 1 };
         }
-
-        // Case where the sequence never reaches 0 is a version of the halting problem
-        // => uncomputable
-
-        // If the value is definitely not in the filter
-        // if next_n & n_filter != next_n || next_n & n_filter != n_filter
     }
+
+    (false, t_main)
 }
 
-fn main() {
-    // Start from 2 as 1 is base case and to save n == 1 check each loop
-    let mut n: u64 = 2; 
-    let mut filter: u64 = 3; // 2 = 0010 => filter = 0011 = 3
+fn main() {    
+    // t_main is the set nodes without even children in main tree T
+    // Nodes without even children are hence described as non-full
+    let mut n: u64 = 2;
+    let mut t_main = HashSet::new();
     let mut counter_example: bool = false;
 
-    while !counter_example {
+    while !counter_example && n < 10000000 {
+        // Can safely skip 1 and 2 as they are trivial. Start with n=3
+        n += 1;
+
+        // If n is part of the 2^m trunk, skip n
+        if (n & (n - 1)) == 0 {
+            continue;
+        }
 
         print!("\rTesting: {}", n);
-        (counter_example, filter) = colatz(n, &filter);
-
-        n += 1;
+        (counter_example, t_main) = colatz_new(n, t_main);  
     }
 
-    println!("Colatz counter example found: {}", n);
+    println!("\n\nColatz counter example found: {}", n);
+    println!("\t_main size: {}", t_main.len());
 }
